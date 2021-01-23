34b8d302afca1a16c6faa2fcf7311095af8cf40f
flyingoldfysh <winnie_x91@hotmail.com>
2021-01-23 17:06:28 +0100

    added relevant API icon

--- src/index.js
+++ src/index.js
@@ -51,14 +51,17 @@ document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.main.temp
   );
-
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-}
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description)
+  }
 searchCity("Canggu");
 
 //Bonus
